23834bd2c5c2162d936113b14e4226d4897d4998
Arun <arunnn101010@gmail.com>
2026-07-18 22:36:36 +0530

    Update Binary search : binary search : solution.py

--- Binary search : binary search : solution.py
+++ Binary search : binary search : solution.py
@@ -1,1 +1,26 @@
+# Promblem - binary serach 
+# Approach - binary serach 
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode4 and diffculty level - 704 & easy 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int left = 0;
+        int right = nums.size() - 1;
 
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else 
+                right = mid - 1;
+        }
+        return -1;
+    }
+};
