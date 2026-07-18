3ec208de691ad4a5275aef9c00e805d44a6072e0
Arun <arunnn101010@gmail.com>
2026-07-18 22:59:01 +0530

    Update Binary search : search insert position : solution.py

--- Binary search : search insert position : solution.py
+++ Binary search : search insert position : solution.py
@@ -1,1 +1,28 @@
+# Promblem - serach insert position 
+# Approach - binary search 
+# Time and space complexity - 0(log n) & 0(1) 
+# Leetcode and diffculty level - 35 & easy 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
 
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            else if(nums[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+};
