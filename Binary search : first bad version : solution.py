28cdf52a7d2c9cf50845e64c68f6c62e10873d8a
Arun <arunnn101010@gmail.com>
2026-07-19 21:38:50 +0530

    Update Binary search : first bad version : solution.py

--- Binary search : first bad version : solution.py
+++ Binary search : first bad version : solution.py
@@ -1,1 +1,30 @@
+# Promblem - first bad version 
+# Approach - binary search 
+# Time and space complexity - 0(log n) & 0(1)
+# Leetcode and diffculty level - 278 & easy 
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long left = 1;
+        long long right = n;
+
+        int ans = n;
+
+        while(left <= right) {
+
+            long long mid = left + (right - left) / 2;
+
+            if(isBadVersion(mid)) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
