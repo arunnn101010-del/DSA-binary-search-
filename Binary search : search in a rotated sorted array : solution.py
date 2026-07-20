0dd795af5058ae56a17142576375d94d7d52561e
Arun <arunnn101010@gmail.com>
2026-07-20 17:19:15 +0530

    Update Binary search : search in a rotated sorted array : solution.py

--- Binary search : search in a rotated sorted array : solution.py
+++ Binary search : search in a rotated sorted array : solution.py
@@ -1,1 +1,38 @@
+# Promblem - search in a rotated sorted array 
+# Approch - binary search 
+# Time and space complexity - 0(log n) & 0(n) 
+# Leetcode and diffculty level - 33 & medium 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0; 
+        int right = nums.size() - 1;
 
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            if(nums[left] <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+        
+            else {
+                if(target < nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
