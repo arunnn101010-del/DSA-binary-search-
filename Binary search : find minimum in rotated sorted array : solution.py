d510b07fa5169f5e69a50b759264872aae9eb245
Arun <arunnn101010@gmail.com>
2026-07-20 21:09:48 +0530

    Update Binary search : find minimum in rotated sorted array : solution.py

--- Binary search : find minimum in rotated sorted array : solution.py
+++ Binary search : find minimum in rotated sorted array : solution.py
@@ -1,1 +1,23 @@
+# Promblem - find minimum in roated sorted array 
+# Approach - binary search 
+# Time and space complexity - 0(log n) & 0(1)
+# Leetcode and diffculty level - 153 & medium 
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
 
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
